c5e78932cbc8a08337c4b69cbe3c60fa110f4360
Tatiana <Bowmantatiana2@gmail.com>
2023-01-28 08:10:19 -0800

    commit tree 0bab78bd99ab3151bd095f80114d3786cb905365

--- methods.rb
+++ methods.rb
@@ -1,1 +1,45 @@
-# Your code here!
+
+# greet_programmer
+def greet_programmer
+  puts  "Hello, programmer!"
+end
+
+greet_programmer
+
+
+#greet
+def greet(name)
+    puts "Hello, #{name}!";
+end
+
+greet("Tatiana")
+
+
+#greet_with_default
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+    greet_with_default("Naureen")
+
+
+    #add
+    def add(num1, num2)
+        num1 + num2
+    end
+
+    puts add(2, 2)
+
+
+    #halve
+
+    def halve(num)
+        if num.class != Integer
+            return nil
+        end
+        num / 2
+    end
+
+    puts halve(4)
+    puts halve("string")
